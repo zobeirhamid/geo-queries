d71084de06a0ec496da58621875895c7c2106dd7
Zobeir Hamid <hamid.zobeir@gmail.com>
2019-08-29 15:40:33 -0700

    Updates exports

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-export GeoQuery, { createGeoQuery } from './src/GeoQuery';
+export GeoQuery from './src/GeoQuery';
 export GeoRegion from './src/GeoRegion';
+export { QueryBuilder } from './src/QueryBuilder';
 
 export default {
   GeoQuery,
-  GeoRegion
+  GeoRegion,
+  QueryBuilder
 };
